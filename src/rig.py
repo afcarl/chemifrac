ce2df52271e5ec1669d6bac7b05bf694d5a95d2a
Jamie Morton <jamietmorton@gmail.com>
2015-12-08 15:37:08 -0800

    Adding in weighting scheme for RIG

--- src/rig.py
+++ src/rig.py
@@ -11,19 +11,26 @@ import pandas as pd
 import copy
 from scipy.sparse.csgraph import dijkstra
 from fractions import Fraction
+from sklearn.metrics.pairwise import pairwise_distances
+from functools import partial
+
+def rig_pairwise(G, X):
+    rig_func = partial(rig, G)
+    # Note cannot work with pandas
+    return pairwise_distances(X, metric=rig_func)
 
 
-def rig(G, x, y):
+def rig(G, x, y, res=1e-9):
     """ Compute the RIG metric on all components.
 
     Parameters
     ----------
     G : nx.Graph
        A connected graph of weighted edges
-    x : list, str
-       Set of nodes present in sample x
-    y : list, str
-       Set of nodes present in sample y
+    x : pd.Series
+       Vector of nodes and their abundance in sample x
+    y : pd.Series
+       Vector of nodes and their abundance in sample y
 
     Returns
     -------
@@ -37,22 +44,49 @@ def rig(G, x, y):
 
     """
     cost = 0
-    for comp in nx.connected_components(G):
-        nodes = set(comp.nodes())
-        cost += rig_component(comp, x & nodes, y & nodes)
-    return cost
+    _G = copy.deepcopy(G)
+    # This converts all of the weights to integers
+    for u,v,d in _G.edges(data=True):
+        d['weight'] = int(d['weight'] / res)
 
-def rig_component(G, x, y):
+    # This calculates the largest edge set to offset the insertion cost.
+    weights = []
+    for comp in nx.connected_component_subgraphs(_G):
+        edges = comp.edges(data='weight')
+        if len(edges) > 0:
+            weights.append(sum(list(zip(*edges))[2]))
+    maxW = max(weights) + 1
+
+    for comp in nx.connected_component_subgraphs(_G):
+        nodes = set(comp.nodes())
+        subx = x[nodes & set(x.keys())]
+        suby = y[nodes & set(y.keys())]
+
+        # Perform closure so that abundances within
+        # each component add up to one
+        if len(subx) > 0:
+            subx = subx / subx.sum()
+        if len(suby) > 0:
+            suby = suby / suby.sum()
+        c = rig_component(comp, subx, suby, maxW)
+        cost += c
+    return (cost)*res
+
+def rig_component(G, x, y, maxW):
     """ Compute the RIG metric on a single component.
 
     Parameters
     ----------
     G : nx.Graph
        A connected graph of weighted edges
-    x : list, str
-       Set of nodes present in sample x
-    y : list, str
-       Set of nodes present in sample y
+    x : pd.Series
+       Vector of nodes and their abundance in sample x
+       within the connected graph G
+    y : pd.Series
+       Vector of nodes and their abundance in sample y
+       within the connected graph G
+    maxW : float
+       The cost of an insertion step
 
     Returns
     -------
@@ -65,146 +99,59 @@ def rig_component(G, x, y):
     If they are both None, then the distance will be zero.
     Also, the weights of the edges must be contained in `'weight'`.
     """
+
+    # Both samples don't contain any metabolites
     if len(x)==0 and len(y)==0:
         return 0
 
+    # Both samples contains the exact same metabolites
+    # Note that this will change once we start adding weights
+    # due to abundance.
+    def equal(x, y):
+        if len(x) != len(y):
+            return False
+        x = x.sort_values(inplace=False)
+        y = y.sort_values(inplace=False)
+
+        d = np.allclose(x.values.astype(np.float),
+                        y.values.astype(np.float))
+        if  d and np.all(x.index==y.index):
+            return True
+        return False
+
+    if equal(x, y):
+        return 0
+
+    # The component being analyzed has only 1 node.
+    # So the networkx simplex algorithm cannot be run
+    # since there are no edges.
+    if len(G.nodes()) == 1:
+        return maxW
+
     cost = 0
     edges = G.edges(data='weight')
-    # This normalizes all of the distances by summing over
-    # all of the edge weights. This is doubled, since edges
-    # can be counted twice
-    weight = sum(list(zip(*edges))[2]) * 2
 
-    # If there is no overlap, arbituarily pick two metabolites
+    # If there one of the samples doesn't have any metabolites
+    # on the component, arbituarily pick two metabolites
     # and append them to the set.  This to address the issue of
-    # measuring distance between unshared components
-    if len(x & y) == 0:
-        x = x | set(list(y)[0])
-        y = y | set(list(x)[0])
-        cost += 1
-
-    if x.issubset(y):
-        alignedG = _subsetted_graph(G, x, y)
-    elif y.issubset(x):
-        alignedG = _subsetted_graph(G, y, x)
+    # measuring distance between unshared components.
+    if len(x) == 0:
+        x = pd.Series({y.index[0]: Fraction(1, 1)})
+        weight = maxW
+    elif len(y) == 0:
+        y = pd.Series({x.index[0]: Fraction(1, 1)})
+        weight = maxW
     else:
-        alignedG = _non_subsetted_graph(G, x, y)
-    W, _ = nx.network_simplex(alignedG.to_directed())
-    cost += W / weight
+        weight = 0
+    _G = copy.deepcopy(G)
+
+    xarr = pd.Series({n:(x[n] if n in x else 0) for n in G.nodes()})
+    yarr = pd.Series({n:(y[n] if n in y else 0) for n in G.nodes()})
+
+    for node in _G.nodes():
+        _G.node[node]['demand'] = xarr[node] - yarr[node]
+
+    W, _ = nx.network_simplex(_G.to_directed())
+
+    cost += W + weight
     return cost
-
-def rig_grow(G, x, y):
-    """ Find the minimum weighted path cover on a single component.
-
-    This is a special case of path cover finding the minimum weighted path
-    cover linking sample x and sample y.  Ultimately, this boils down to the
-    mininum flow problem, which can be solved via network simplex algorithm.
-
-    Parameters
-    ----------
-    G : nx.Graph
-       A connected graph of weighted edges
-    x : list, str
-       Set of nodes present in sample x
-    y : list, str
-       Set of nodes present in sample y
-
-    Returns
-    -------
-    float :
-       Distance between sample x and sample y
-
-    Note
-    ----
-    If x or y is None, then 1 will be added to the total distance.
-    If they are both None, then the distance will be zero.
-
-    Also, this module as it stands is only dealing with presence/absence
-
-    """
-    if x.issubset(y):
-        alignedG = _subsetted_graph(G, x, y)
-    elif y.issubset(x):
-        alignedG = _subsetted_graph(G, y, x)
-    else:
-        alignedG = _non_subsetted_graph(G, x, y)
-    cost, edges = nx.network_simplex(alignedG.to_directed())
-    return cost
-
-def _subsetted_graph(G, x, y):
-    """ Finds mininum flow cover in the scenario where x is a subset of y
-
-    This is a special case of path cover finding the minimum weighted path
-    cover linking sample x and sample y.  Ultimately, this boils down to the
-    mininum flow problem, which can be solved via network simplex algorithm.
-
-    Parameters
-    ----------
-    G : nx.Graph
-       A connected graph of weighted edges
-    x : list, str
-       Set of nodes present in sample x.  x is a subset of y
-    y : list, str
-       Set of nodes present in sample y.  y is a superset of x
-
-    Returns
-    -------
-    nx.Graph :
-       Aligned graph
-
-    """
-    xtotal = len(x)
-
-    inX, outX = x, y - x
-    newG = copy.deepcopy(G)
-    for node in G.nodes():
-        if node in inX:
-            newG.node[node]['demand'] = Fraction(1, xtotal)
-        else:
-            newG.node[node]['demand'] = Fraction(-1, xtotal)
-    return newG
-
-def _non_subsetted_graph(G, x, y):
-    """ Finds mininum flow cover in the scenario where x is not a subset of y
-
-    This is a special case of path cover finding the minimum weighted path
-    cover linking sample x and sample y.  Ultimately, this boils down to the
-    mininum flow problem, which can be solved via network simplex algorithm.
-
-    Parameters
-    ----------
-    G : nx.Graph
-       A connected graph of weighted edges
-    x : list, str
-       Set of nodes present in sample x.  x is a subset of y
-    y : list, str
-       Set of nodes present in sample y.  y is a superset of x
-
-    Returns
-    -------
-    nx.Graph :
-       Aligned graph
-
-    """
-    Gx = copy.deepcopy(G)
-    Gy = copy.deepcopy(G)
-    num_shared = len(x & y)
-    num_uniquex = len(x - y)
-    num_uniquey = len(y - x)
-    xtotal = len(x)
-    ytotal = len(y)
-    for node in G.nodes():
-        if node in x and node not in y:
-            Gx.node[node]['demand'] = Fraction(1, xtotal)
-            Gy.node[node]['demand'] = Fraction(-1, num_uniquex)
-        elif node not in x and node in y:
-            Gx.node[node]['demand'] = Fraction(-1, num_uniquey)
-            Gy.node[node]['demand'] = Fraction(1, ytotal)
-        elif node in x and node in y:
-            Gx.node[node]['demand'] = Fraction(1, xtotal)
-            Gy.node[node]['demand'] = Fraction(1, ytotal)
-        else:
-            Gx.node[node]['demand'] = 0
-            Gy.node[node]['demand'] = 0
-    alignedG = nx.union(Gx, Gy, rename=('x-','y-') )
-    return alignedG
--- src/tests/test_rig.py
+++ src/tests/test_rig.py
@@ -1,54 +1,74 @@
-from src.rig import rig_component, rig_grow, rig
+from src.rig import rig_component,  rig
+import pandas as pd
 import numpy as np
 import networkx as nx
 import unittest
 from fractions import Fraction
 
-class TestGrowth(unittest.TestCase):
+class TestRig(unittest.TestCase):
 
-    def test_rig_grow1(self):
+    def test_rig(self):
         G = nx.Graph()
         G.add_nodes_from(['i', 'j', 'k'])
-        G.add_edge('i','j', {'weight': 1})
-        G.add_edge('j','k', {'weight': 10})
-        res = rig_grow(G, {'i'}, {'k'})
-        self.assertEquals(int(res), 22)
+        G.add_edge('i','j', {'weight': .9})
 
-    def test_rig_grow2(self):
+        a1 = rig(G,
+                 pd.Series({'i':1, 'j':1, 'k':1}),
+                 pd.Series({'i':1, 'j':1, 'k':1}))
+        a2 = rig(G,
+                 pd.Series({'j':Fraction(1, 2),
+                            'k':Fraction(1, 2)}),
+                 pd.Series({'i':Fraction(1, 3),
+                            'j':Fraction(1, 3),
+                            'k':Fraction(1, 3)}))
+        a3 = rig(G,
+                 pd.Series({'j':1,'k':1}),
+                 pd.Series({'i':1,'k':1}))
+        a4 = rig(G,
+                 pd.Series({'i':Fraction(1, 2),
+                            'j':Fraction(1, 2)}),
+                 pd.Series({'i':Fraction(1, 3),
+                            'j':Fraction(1, 3),
+                            'k':Fraction(1, 3)}))
+        a5 = rig(G,
+                 pd.Series({'i':1,'k':1}),
+                 pd.Series({'i':1,'j':1}))
+
+        a6 = rig(G,
+                 pd.Series({'i':Fraction(1, 3),
+                            'j':Fraction(1, 3),
+                            'k':Fraction(1, 3)}),
+                 pd.Series({'j':1}))
+        a7 = rig(G,
+                 pd.Series({'i':Fraction(1, 3),
+                            'j':Fraction(1, 3),
+                            'k':Fraction(1, 3)}),
+                 pd.Series({'k':1}))
+        a8 = rig(G,
+                 pd.Series({'i':Fraction(1, 2),
+                            'k':Fraction(1, 2)}),
+                 pd.Series({'j':1}))
+        a9 = rig(G,
+                 pd.Series({'i':Fraction(1, 2),
+                            'j':Fraction(1, 2)}),
+                 pd.Series({'k':1}))
+
+        self.assertLessEqual(a1, a2)
+        self.assertLessEqual(a2, a3)
+        self.assertLessEqual(a3, a4)
+        self.assertLessEqual(a4, a5)
+        self.assertLessEqual(a5, a6)
+        self.assertLessEqual(a6, a7)
+        self.assertLessEqual(a7, a8)
+        self.assertLessEqual(a8, a9)
+
+    def test_rig_pairwise(self):
         G = nx.Graph()
         G.add_nodes_from(['i', 'j', 'k'])
-        G.add_edge('i','j', {'weight': 1})
-        G.add_edge('j','k', {'weight': 10})
-        res = rig_grow(G, {'i', 'j'}, {'k'})
-        self.assertEquals(int(res), 21)
-
-    def test_rig_grow3(self):
-        G = nx.Graph()
-        G.add_nodes_from(['i', 'j', 'k', 'l'])
-        G.add_edge('i','j', {'weight': 1})
-        G.add_edge('j','k', {'weight': 10})
-        G.add_edge('k','l', {'weight': 20})
-        res = rig_grow(G, {'i', 'j'}, {'j', 'k', 'l'})
-        self.assertEquals(res, Fraction(209, 6))
-
-    def test_rig_grow4(self):
-        G = nx.Graph()
-        G.add_nodes_from(['i', 'j', 'k', 'l'])
-        G.add_edge('i','j', {'weight': 1})
-        G.add_edge('j','k', {'weight': 10})
-        G.add_edge('k','l', {'weight': 20})
-        res = rig_grow(G, {'i'}, {'j', 'k', 'l'})
-        self.assertEquals(res, Fraction(86, 3))
-
-    def test_rig_grow5(self):
-        # Tests the scenario where x is a subset of y
-        G = nx.Graph()
-        G.add_nodes_from(['i', 'j', 'k', 'l'])
-        G.add_edge('i','j', {'weight': 1})
-        G.add_edge('j','k', {'weight': 10})
-        G.add_edge('k','l', {'weight': 20})
-        res = rig_grow(G, {'i', 'l'}, {'i', 'j', 'k', 'l'})
-        self.assertEquals(res, Fraction(21, 2))
+        G.add_edge('i','j', {'weight': .9})
+        X = np.array([[1, 1, 1],
+                      [1, 1, 2]])
+        rig_pairwise(G, X)
 
 if __name__=='__main__':
     unittest.main()
